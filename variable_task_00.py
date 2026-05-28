fa797e9f9fbe58016f891ad6ba6f10d8ed99fe3b
Dilmurod_Ravshanov <169117371+DilmurodDev-FS@users.noreply.github.com>
2026-05-28 20:55:29 +0500

    Code added to variable_task_00.py file

---
+++ variable_task_00.py
@@ -0,0 +1,24 @@
+# Task given by Lady Claudy
+x = 5
+y = 3
+x = x + y
+#print(x)
+
+# Task given by Lady Claudy
+ism = "Lady Claudy"
+yosh = 21
+#print(yosh)
+#print(ism)
+
+# Task given by Lady Claudy
+son = 2
+#print(son)
+son = 5
+#print(son)
+
+ismim = "Dilmurod"
+familyam = "Ravshanov"
+yoshim = 21
+print(F"Asalomu alaykum, mening ismim {ismim} {familyam} va yoshim {yoshim}")
+
+
