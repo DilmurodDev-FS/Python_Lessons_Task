288e4aa75a188a1c38812d57cfbee380ece3495e
Dilmurod_Ravshanov <169117371+DilmurodDev-FS@users.noreply.github.com>
2026-05-28 21:06:44 +0500

    The string_task_oo.py file has been added and the codes have been written.

---
+++ string_task_00.py
@@ -0,0 +1,17 @@
+# Task given by Lady Claudy
+
+salom = "Assalomu alaykum"
+#print(salom)
+
+matn = "Python dasturlash tili"
+#print(len(matn))
+
+#das_nomi = "Python"
+#print(das_nomi.upper())
+
+s0 = "Salom"
+s1 = "Dunyo"
+#print(s0 + " " + s1)
+
+das_nomi = "PYTHON"
+#print(das_nomi.lower())
