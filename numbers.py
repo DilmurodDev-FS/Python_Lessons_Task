d1a4d68caa6410990f551ae6316f9f8f8ad88a97
Dilmurod_Ravshanov <169117371+DilmurodDev-FS@users.noreply.github.com>
2026-05-29 17:22:25 +0500

    The numbers.py file has been added and exercises on the topic have been completed.

---
+++ numbers.py
@@ -0,0 +1,31 @@
+# Foydalanuvchi kiritgan sonni teskari tartibda chiqaruvchi dastur
+
+""" son = input("Son kiriting: ")
+teskari_son = son[::-1]
+print("Teskari tartibda son:", teskari_son) """
+
+# Ikki sonni o'rta arifmetigini hisoblayidgan dastur
+""" son1 = float(input("Birinchi sonni kiriting: "))
+son2 = float(input("Ikkinchi sonni kiriting: "))
+orta_arifmetik = (son1 + son2) / 2
+print("Ikki sonning o'rta arifmetigi:", orta_arifmetik) """
+
+#Narx va chegirma Mahsulot narxi va chegirma foizini kiritib, to'lanadigan summani hisoblayidigan dastur.
+""" narx = float(input("Mahsulot narxini kiriting: " ))
+chegirma_foizi = float(input("Chegirma foizini kiriting: " ))
+chegirma = narx * chegirma_foizi / 100
+tolanadigan_summa = narx - chegirma
+print("To'lanadigan summa:", tolanadigan_summa) """
+
+# Foydalanuvchi kiritgan 3 xonali sonning raqamlar yig'indisini hisoblovchi dastur
+""" son = input("3 xonali son kiriting: ")
+a = int(son[0])
+b = int(son[1])
+c = int(son[2])
+yigindi = a + b + c
+print("Kiritgan 3 xonali soningizning raqamlar yig'indisi:", yigindi) """
+
+#Foydalanuvchi nechchi kun yashaganini hisoblovchi dastur
+yosh = int(input("Yoshingizni kiriting: "))
+kunlar = yosh * 365
+print(f"Siz tahminan {kunlar} kun yashagansiz.")
